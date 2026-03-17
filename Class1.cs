a76d7299cc385da5ea64a37ce61f016fa46eea9e
d11416148-sys <d11416148@gs.takming.edu.tw>
2026-03-17 15:37:40 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Test
+{
+	public int mid;
+	private int final;
+
+	public void SetFinal(int value)
+	{
+		final = value;
+	}
+
+	public int GetFinal()
+	{
+		return final;
+	}
+	public double GetAvg()
+	{ 
+		return(mid +  final)/2.0;
+	}
+
+	public void SetGrade(int m, int f)
+	{
+		if (ValideGrade(m, f))
+		{
+			mid = m;
+			final = f;
+		}
+	}
+	private bool ValideGrade(int m, int f)
+	{
+
+		if (m >= 0 && f <= 100)
+		{
+			if (f <= 0 && f <= 100)
+			{ return true; }
+		}
+		return false;
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace class_0317_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test std1, std2;
+
+            std1 = new Test();
+            std2 = new Test();
+
+            std1.mid = 78;
+            std1.SetFinal(72);
+
+            std2.SetGrade(68, 84);
+
+            Console.WriteLine(std1.GetAvg().ToString());
+            Console.WriteLine(std2.GetAvg().ToString());
+        }
+    }
+}
